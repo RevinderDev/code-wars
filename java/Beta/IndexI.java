6910080ac535e193977a66c1b9e20182909db3ac
Michael Kasprzyk <michal0kasprzyk@gmail.com>
2020-09-04 20:12:19 +0200

    [Java] Beta Index i

---
+++ java/Beta/IndexI.java
@@ -0,0 +1,13 @@
+//Look at the tests... what do you need?
+
+public class Array{
+
+
+
+  public static Object[] array = new Object[10];
+
+  public static Object array(int index)
+  {
+    return Array.array[index];
+  }
+}
---
+++ java/Beta/README.md
@@ -0,0 +1,5 @@
+### About
+
+This folder contains katas that were not ranked yet, thus they are of various difficulties.
+
+1. [Index I](https://www.codewars.com/kata/5def492088417400010b0a7f)
